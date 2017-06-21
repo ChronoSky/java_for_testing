620756001aa6754c83e746cc6995f5c7a6fe3a65
ChronoSky <zik2004@mail.ru>
2017-06-21 14:24:20 +0400

    Задание № 3 (update)

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -8,6 +8,15 @@ public class PointTests {
 
     @Test
     public void testArea(){
+        Point p1 = new Point(0.0, 0.0);
+        Point p2 = new Point(3.0, 4.0);
+
+        Assert.assertEquals(p1.distance(p2) , 5.0);
+    }
+
+
+    @Test
+    public void testArea2(){
         Point p1 = new Point(2.0, 3.0);
         Point p2 = new Point(5.0, 7.0);
 

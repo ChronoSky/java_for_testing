4dcda51cad17b5cd90849b3ae8100867f8159a7e
ChronoSky <zik2004@mail.ru>
2017-06-27 00:34:14 +0400

    Задание №6 Создал тест модификации для групп Создал недостающие тесты (удаление/модификация) для контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -29,4 +29,25 @@ public class ContactHelper extends HelperBase{
         type(By.name("home"),contactData.getPhone());
     }
 
+    public void selectContact() {
+        if (!wd.findElement(By.name("selected[]")).isSelected()) {
+            click(By.name("selected[]"));
+        }
+    }
+
+    public void deleteSelectedContacts() {
+        click(By.xpath("//div[@id='content']/form[2]/div[2]/input"));
+    }
+
+    public void acceptAlert() {
+        wd.switchTo().alert().accept();
+    }
+
+    public void initContactModification() {
+        click(By.xpath("//table[@id='maintable']/tbody/tr[2]/td[8]/a/img"));
+    }
+
+    public void submitContactModification() {
+        click(By.name("update"));
+    }
 }

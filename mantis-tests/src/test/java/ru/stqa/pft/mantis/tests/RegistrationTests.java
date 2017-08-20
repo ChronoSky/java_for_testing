bcc783b8330b80210a172197ab8b941b64b637ba
ChronoSky <zik2004@mail.ru>
2017-08-20 21:26:02 +0400

    реализовано: -Прямое взаимодействие с сервером по протоколу HTTP -Ленивая инициализация помошников -Передача файлов на удаленную машину по протоколу FTP -Получение почты на встроенный в тесты почтовый сервер

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
@@ -1,12 +1,45 @@
 package ru.stqa.pft.mantis.tests;
 
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import ru.lanwen.verbalregex.VerbalExpression;
+import ru.stqa.pft.mantis.model.MailMessage;
+
+import javax.mail.MessagingException;
+import java.io.IOException;
+import java.util.List;
 
 public class RegistrationTests extends TestBase{
 
-    @Test
-    public void testRegistretion(){
-        app.registation().start("user1","user1@localhost.localdomain");
+   @BeforeMethod
+    public void startMailServer(){
+        app.mail().start();
     }
+
+    @Test
+    public void testRegistretion() throws IOException, MessagingException {
+        long now = System.currentTimeMillis();
+        String user = String.format("user_%s" , now);
+        String password = "password";
+        String email = String.format("user_%s@localhost.localdomain",now);
+        app.registation().start(user, email);
+        List<MailMessage> mailMassages = app.mail().waitForMail(2, 10000);
+        String confirmationLink = findConfirmationLink(mailMassages, email);
+        app.registation().finish(confirmationLink, password);
+        app.newSession().login(user, password);
+    }
+
+    private String  findConfirmationLink(List<MailMessage> mailMassages, String email) {
+        MailMessage mailMessage = mailMassages.stream().filter((m) -> m.to.equals(email)).findFirst().get();
+        VerbalExpression regex = VerbalExpression.regex().find("http://").nonSpace().oneOrMore().build();
+        return regex.getText(mailMessage.text);
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void stopMailServer(){
+        app.mail().stop();
+    }
+
 }
